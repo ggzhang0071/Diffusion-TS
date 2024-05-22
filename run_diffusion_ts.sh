d231a3fc88b15224c5217f06b02d2aca8ce3e323
zhanggg <zhanggg@zhaomei.com>
2024-05-22 10:50:36 +0800

    add gluonts forecast demo

---
+++ run_diffusion_ts.sh
@@ -0,0 +1,26 @@
+checkpoint_number=10
+pred_len=24
+timestamp=`date +%Y%m%d%H%M%S`
+logfile="Logs/${timestamp}.log"
+rm Logs/*.log
+#"energy" "stocks"  "mujoco"  
+for dataset_name in  "energy" "stocks"  "mujoco"  
+do 
+#echo "Running prediction for dataset: $dataset_name"
+
+
+#python main.py ---name ${dataset_name}  --config_file ./Config/${dataset_name}.yaml --gpu 1 --train
+
+
+echo "Running prediction for dataset: $dataset_name"   2>&1 |tee -a  ${logfile}
+
+python  main.py --name ${dataset_name} --config_file ./Config/${dataset_name}.yaml \
+--gpu 1 --sample 1 --milestone $checkpoint_number  --tensorboard --mode predict --pred_len ${pred_len}  2>&1 |tee -a  ${logfile}
+
+
+done 
+
+
+
+
+
